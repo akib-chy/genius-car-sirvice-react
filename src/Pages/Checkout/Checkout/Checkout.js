e15bf6365f526b677dedeca5d49b9599587ad7f2
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-26 20:18:59 +0600

    proced chackout update

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import useServices from "../../../Hooks/useServices";
 
 const Checkout = () => {
-    return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
-    );
+  const { serviceId } = useParams();
+  const [services] = useServices(serviceId);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const pass = e.target.pass.value;
+    const user = { email, pass };
+    console.log(user);
+  };
+  return (
+    <div className="w-50 mx-auto my-5 shadow p-4">
+      <h2 className="text-center my-4">Please Book: {services.name}</h2>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            name="email"
+            type="email"
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            required
+            name="pass"
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import useServices from "../../Hooks/useServices";
 
 const ServiceDetail = () => {
   const { serviceId } = useParams();
-  const [service, setService] = useState({});
-  useEffect(() => {
-    const url = `http://localhost:5000/service/${serviceId}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setService(data));
-  }, []);
+  const [services] = useServices(serviceId);
+  // const [service, setService] = useState({});
+  // useEffect(() => {
+  //   const url = `http://localhost:5000/service/${serviceId}`;
+  //   fetch(url)
+  //     .then((res) => res.json())
+  //     .then((data) => setService(data));
+  // }, []);
   return (
     <div>
-      <h2>Welcome to detail: {service.name}</h2>
+      <h2>Welcome to detail: {services.name}</h2>
       <div className="text-center">
-        <Link to="/checkout">
+        <Link to={`/checkout/${serviceId}`}>
           <button className="btn btn-primary">Proceed Checkout</button>
         </Link>
       </div>
