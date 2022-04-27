6e4baae1f95081baa215e53e2cc544a87a8ffa3d
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-27 12:43:45 +0600

    user information from added

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,19 +1,28 @@
-import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import useServices from "../../../Hooks/useServices";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
   const { serviceId } = useParams();
   const [services] = useServices(serviceId);
-  const [user, setUser] = useState({
-    name: "Minhajul Alam",
-    email: "akibchy1212@gmail.com",
-    phone: "01554101441",
-    address: "fatickchari, chittagong",
-  });
-  const handleAddressChaing = (e) => {
-    // e.preventDefault();
+  const [user] = useAuthState(auth);
+  // const [user, setUser] = useState({
+  //   name: "Minhajul Alam",
+  //   email: "akibchy1212@gmail.com",
+  //   phone: "01554101441",
+  //   address: "fatickchari, chittagong",
+  // });
+  const handleOrderSubmit = (e) => {
+    e.preventDefault();
+    const order = {
+      name: user.displayName,
+      email: user.email,
+      service: serviceId.name,
+      phone: e.target.phone.value,
+      address: e.target.address.value,
+    };
     // const name = e.target.name.value;
     // const email = e.target.email.value;
     // const service = e.target.service.value;
@@ -21,24 +30,24 @@ const Checkout = () => {
     // const address = e.target.address.value;
     // const user = { name, email, service, phone, address };
     // console.log(user);
-    const { address, ...rest } = user;
-    const newAddress = e.target.value;
-    const newUser = { address: newAddress, ...rest };
-    console.log(newUser);
-    setUser(newUser);
   };
+  // const { address, ...rest } = user;
+  // const newAddress = e.target.value;
+  // const newUser = { address: newAddress, ...rest };
+  // console.log(newUser);
+  // setUser(newUser);
   return (
     <div className="w-50 mx-auto my-5 shadow p-4">
       <h2 className="text-center my-4">Please Book: {services.name}</h2>
-      <Form>
+      <Form onSubmit={handleOrderSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
           <Form.Control
-            value={user.name}
+            value={user.displayName}
             required
             name="name"
             type="text"
-            placeholder="Name"
+            disabled
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -47,7 +56,7 @@ const Checkout = () => {
             required
             name="email"
             type="email"
-            placeholder="Enter email"
+            disabled
             value={user.email}
           />
         </Form.Group>
@@ -68,19 +77,16 @@ const Checkout = () => {
             name="phone"
             type="tel"
             placeholder="Phone Number"
-            value={user.phone}
           />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Address</Form.Label>
           <Form.Control
-            onChange={handleAddressChaing}
             required
             name="address"
             type="text"
             placeholder="Address"
-            value={user.address}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
