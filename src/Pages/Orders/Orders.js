9e94328b449d80ddfb1c66dd7cac5efbdbaab00c
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-27 19:32:18 +0600

    order update

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,15 +1,32 @@
+import axios from "axios";
+import { signOut } from "firebase/auth";
 import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 const Orders = () => {
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
   const [orders, setOrders] = useState([]);
   useEffect(() => {
-    const email = user?.email;
-    const url = `http://localhost:5000/orders?email=${email}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setOrders(data));
+    const getOrder = async () => {
+      try {
+        const email = user?.email;
+        const url = `http://localhost:5000/orders?email=${email}`;
+        const { data } = await axios.get(url, {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        });
+        setOrders(data);
+      } catch (error) {
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate("/login");
+        }
+      }
+    };
+    getOrder();
   }, [user]);
   return (
     <div>
